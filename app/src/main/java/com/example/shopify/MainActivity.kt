0f8778c63c771d9b75f0b8b0eefc85267f3ccad6
ehsan <ahmednoha771@gmail.com>
2023-07-16 18:49:00 +0300

    test branch

--- app/src/main/java/com/example/shopify/MainActivity.kt
+++ app/src/main/java/com/example/shopify/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("TAG", "onCreate: fffffffffffffff")
+        Log.i("TAG", "onCreate: noha")
+
     }
 }
