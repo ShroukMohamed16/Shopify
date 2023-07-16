dde9d36304d8a436a7975de1262034c4a5bb63d3
ehsan <ahmednoha771@gmail.com>
2023-07-16 19:21:44 +0300

    Adding HomeActivity

--- app/src/main/java/com/example/shopify/splash/SplashScreen.kt
+++ app/src/main/java/com/example/shopify/splash/SplashScreen.kt
@@ -1,12 +1,23 @@
 package com.example.shopify.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.shopify.R
+import com.example.shopify.homeActivity.HomeActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        Handler().postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 1000)
     }
 }
