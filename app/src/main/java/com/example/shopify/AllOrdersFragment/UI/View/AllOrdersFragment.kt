956dca6b3827cc3d107ecd309e65d4c2ad031117
ehsan <ahmednoha771@gmail.com>
2023-07-20 01:11:38 +0300

    adding AllOrder Fragment

---
+++ app/src/main/java/com/example/shopify/AllOrdersFragment/UI/View/AllOrdersFragment.kt
@@ -0,0 +1,21 @@
+package com.example.shopify.AllOrdersFragment.UI.View
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shopify.R
+
+class AllOrdersFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_orders, container, false)
+    }
+
+}
