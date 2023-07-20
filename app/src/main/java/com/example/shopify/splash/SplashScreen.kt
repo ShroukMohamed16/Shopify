3daa5cd5d7e9489f1f4723f3f24b5b64034cd8d2
BassantMohammed788 <100326081+BassantMohammed788@users.noreply.github.com>
2023-07-21 02:26:28 +0300

    handling coupons

--- app/src/main/java/com/example/shopify/splash/SplashScreen.kt
+++ app/src/main/java/com/example/shopify/splash/SplashScreen.kt
@@ -3,12 +3,12 @@ package com.example.shopify.splash
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.shopify.OnBoarding.OnBoardingActivity
 import com.example.shopify.R
 import com.example.shopify.authentication.AuthenticationActivity
 import com.example.shopify.homeActivity.HomeActivity
+import com.example.shopify.utilities.MySharedPreferences
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -22,11 +22,19 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        GlobalScope.launch(Dispatchers.Main) {
+        if(!MySharedPreferences.getInstance(this@SplashScreen).getOnBoardingState()){
+            GlobalScope.launch(Dispatchers.Main) {
+
             delay(4000)
+               val intent = Intent(this@SplashScreen, OnBoardingActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
 
-
-                /*if(auth.currentUser == null){
+        }else{
+            GlobalScope.launch(Dispatchers.Main) {
+                delay(4000)
+                if(auth.currentUser == null){
                     val intent = Intent(this@SplashScreen, AuthenticationActivity::class.java)
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
                     startActivity(intent)
@@ -34,11 +42,8 @@ class SplashScreen : AppCompatActivity() {
                     val intent = Intent(this@SplashScreen, HomeActivity::class.java)
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
                     startActivity(intent)
-                }*/
-            val intent = Intent(this@SplashScreen, OnBoardingActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-
+                }
+            }
         }
     }
 
