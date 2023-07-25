a0873e5c1b2ee69b589900b10ceb69af1af514f6
Shrouk Mohamed <96442266+ShroukMohamed16@users.noreply.github.com>
2023-07-25 13:07:40 +0300

    Create Draft Order

--- app/src/main/java/com/example/shopify/utilities/Constants.kt
+++ app/src/main/java/com/example/shopify/utilities/Constants.kt
@@ -5,6 +5,11 @@ object Constants {
     const val PREFERENCE_COUPON_CODE : String = "couponName"
     const val DESTINATION : String = "destination"
     const val ONBOARDING_FLAG : String  = "onboarding_flag"
+    const val CUSTOMER_ID = "CustomerID"
+    const val CART_DRAFT_ID="CartID"
+    const val FAV_DRAFT_ID="FavouriteID"
+    const val CART_NOTE = "cart"
+    const val FAV_NOTE = "favourite"
     var first_name =""
     var last_name =""
     var CustomerListResponseSize = 0
--- app/src/main/java/com/example/shopify/utilities/MySharedPrefrences.kt
+++ app/src/main/java/com/example/shopify/utilities/MySharedPrefrences.kt
@@ -27,6 +27,25 @@ class MySharedPreferences private constructor(context: Context) {
     fun getCouponCode(): String? {
         return sharedPreferences.getString(Constants.PREFERENCE_COUPON_CODE, null)
     }
+    fun saveCustomerID(id: Long) {
+        sharedPreferences.edit().putLong(Constants.CUSTOMER_ID, id).apply()
+    }
+    fun getCartID(): Long? {
+        return sharedPreferences.getLong(Constants.CART_DRAFT_ID, 0)
+    }
+    fun saveCartID(id: Long) {
+        sharedPreferences.edit().putLong(Constants.CART_DRAFT_ID, id).apply()
+    }
+    fun getFavID(): Long? {
+        return sharedPreferences.getLong(Constants.FAV_DRAFT_ID, 0)
+    }
+    fun saveFavID(id: Long) {
+        sharedPreferences.edit().putLong(Constants.FAV_DRAFT_ID, id).apply()
+    }
+
+    fun getCustomerID(): Long? {
+        return sharedPreferences.getLong(Constants.CUSTOMER_ID, 0)
+    }
 
     fun saveOnBoardingState(state:Boolean){
         sharedPreferences.edit().putBoolean(Constants.ONBOARDING_FLAG, state).apply()
